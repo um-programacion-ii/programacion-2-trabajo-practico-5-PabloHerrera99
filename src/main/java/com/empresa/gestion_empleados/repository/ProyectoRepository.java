924949a9c2b2c31e453f0e0f5ffdad65c307169c
PabloHerrera99 <p.herrera@alumno.um.edu.ar>
2025-08-27 12:14:12 -0300

    Se crearon las consultas para empleado y proyecto

--- src/main/java/com/empresa/gestion_empleados/repository/ProyectoRepository.java
+++ src/main/java/com/empresa/gestion_empleados/repository/ProyectoRepository.java
@@ -2,6 +2,15 @@ package com.empresa.gestion_empleados.repository;
 
 import com.empresa.gestion_empleados.entity.Proyecto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ProyectoRepository extends JpaRepository<Proyecto, Long>{
+    List<Proyecto> findProyectoActivo(String nombre);
+
+    @Query("SELECT p from Proyecto p WHERE p.fechaFin > :hoy")
+    List<Proyecto> findProyectoActivo(@Param("hoy") LocalDate hoy);
 }
